65d4b0776ba8431f5aff971ae359b348ec302e7b
Ник Николос <nic23172@gmail.com>
2021-03-05 18:18:23 +0300

    feat: create handles for popup

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,15 +8,15 @@ export default function Main() {
       <div className="profile__container">
         <div className="profile__avatar-cover">
           <img className="profile__avatar" src={avatar} alt="Аватар"/>
-          <button className="profile__avatar-button" type="button" aria-label="edit"></button>
+          <button className="profile__avatar-button" type="button" aria-label="edit" onClick={handleEditAvatarClick}></button>
         </div>
         <div className="profile__info">
           <h1 className="profile__name">Жак-Ив Кусто</h1>
           <p className="profile__job">Исследователь океана</p>
-          <button className="profile__edit-button" type="button" aria-label="edit"></button>
+          <button className="profile__edit-button" type="button" aria-label="edit" onClick={handleEditProfileClick}></button>
         </div>
       </div>
-      <button className="profile__add-button" type="button" aria-label="add"></button>
+      <button className="profile__add-button" type="button" aria-label="add" onClick={handleAddPlaceClick}></button>
     </section>
 
     <section className="elements content__elements">
@@ -36,4 +36,19 @@ export default function Main() {
     </section>
   </main>
   );
+}
+
+function handleEditAvatarClick() {
+  const popupAvatar = document.querySelector('.popup_refresh');
+  popupAvatar.classList.add('popup_opened');
+}
+
+function handleEditProfileClick() {
+  const popupEdit = document.querySelector('.popup_edd');
+  popupEdit.classList.add('popup_opened');
+}
+
+function handleAddPlaceClick() {
+  const popupAdd = document.querySelector('.popup_add');
+  popupAdd.classList.add('popup_opened');
 }
