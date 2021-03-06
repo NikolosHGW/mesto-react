bd6d4d1a8da7c174218cd7ed2c7d20a58f139645
Ник Николос <nic23172@gmail.com>
2021-03-06 09:47:53 +0300

    fix: fix gitm commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,81 +2,76 @@ import Header from '../components/Header.js';
 import Main from '../components/Main.js';
 import Footer from '../components/Footer.js';
 import ImagePopup from '../components/ImagePopup.js';
+import PopupWithForm from '../components/PopupWithForm.js';
 
 function App() {
   return (
   <div className="body">
     <div className="page">
       <Header />
-      <Main />
+      <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick} />
       <Footer />
     </div>
 
-    <div className="popup popup_edd">
-      <div className="popup__container">
-        <button className="popup__close-icon" type="button" aria-label="close"></button>
-        <h2 className="popup__heading">Редактировать профиль</h2>
-        <form className="popup__form" name="popupForm" noValidate>
-          <fieldset className="popup__input-text">
-            <label className="popup__field">
-              <input id="name-input" className="popup__input popup__input_el_name" type="text" name="name" required minLength="2" maxLength="40"/>
-              <span className="popup__input-error name-input-error"></span>
-            </label>
-            <label className="popup__field">
-              <input id="job-input" className="popup__input popup__input_el_job" type="text" name="job" required minLength="2" maxLength="200"/>
-              <span className="popup__input-error job-input-error"></span>
-            </label>
-          </fieldset>
-          <button className="popup__save-button" type="submit">Сохранить</button>
-        </form>
-      </div>
-    </div>
-    <div className="popup popup_add">
-      <div className="popup__container">
-        <button className="popup__close-icon" type="button" aria-label="close"></button>
-        <h2 className="popup__heading">Новое место</h2>
-        <form className="popup__form" name="popupAddForm" noValidate>
-          <fieldset className="popup__input-text">
-            <label className="popup__field">
-              <input id="card-name-input" className="popup__input popup__input_el_card-name" placeholder="Название" type="text" name="name" required minLength="2" maxLength="30"/>
-              <span className="popup__input-error card-name-input-error"></span>
-            </label>
-            <label className="popup__field">
-              <input id="img-link-input" className="popup__input popup__input_el_img-link" placeholder="Ссылка на картинку" type="url" name="link" required/>
-              <span className="popup__input-error img-link-input-error"></span>
-            </label>
-          </fieldset>
-          <button className="popup__save-button" type="submit">Создать</button>
-        </form>
-      </div>
-    </div>
+    <PopupWithForm name="edd"
+    title="Редактировать профиль" textButton="Сохранить">
+      <fieldset className="popup__input-text">
+        <label className="popup__field">
+          <input id="name-input" className="popup__input popup__input_el_name" type="text" name="name" required minLength="2" maxLength="40"/>
+          <span className="popup__input-error name-input-error"></span>
+        </label>
+        <label className="popup__field">
+          <input id="job-input" className="popup__input popup__input_el_job" type="text" name="job" required minLength="2" maxLength="200"/>
+          <span className="popup__input-error job-input-error"></span>
+        </label>
+      </fieldset>
+    </PopupWithForm>
+
+    <PopupWithForm name="add"
+    title="Новое место" textButton="Создать">
+      <fieldset className="popup__input-text">
+        <label className="popup__field">
+          <input id="card-name-input" className="popup__input popup__input_el_card-name" placeholder="Название" type="text" name="name" required minLength="2" maxLength="30"/>
+          <span className="popup__input-error card-name-input-error"></span>
+        </label>
+        <label className="popup__field">
+          <input id="img-link-input" className="popup__input popup__input_el_img-link" placeholder="Ссылка на картинку" type="url" name="link" required/>
+          <span className="popup__input-error img-link-input-error"></span>
+        </label>
+      </fieldset>
+    </PopupWithForm>
+
     <ImagePopup />
-    <div className="popup popup_del">
-      <div className="popup__container">
-        <button className="popup__close-icon" type="button" aria-label="close"></button>
-        <h2 className="popup__heading">Вы уверены?</h2>
-        <form className="popup__form popup__form_del" name="popupDelForm" noValidate>
-          <button className="popup__save-button popup__save-button_del" type="submit">Да</button>
-        </form>
-      </div>
-    </div>
-    <div className="popup popup_refresh">
-      <div className="popup__container">
-        <button className="popup__close-icon" type="button" aria-label="close"></button>
-        <h2 className="popup__heading">Обновить аватар</h2>
-        <form className="popup__form" name="popupRefreshForm" noValidate>
-          <fieldset className="popup__input-text">
-            <label className="popup__field">
-              <input id="avatar-link-input" className="popup__input popup__input_el_avatar-link" placeholder="Ссылка на картинку" type="url" name="link" required/>
-              <span className="popup__input-error avatar-link-input-error"></span>
-            </label>
-          </fieldset>
-          <button className="popup__save-button" type="submit">Сохранить</button>
-        </form>
-      </div>
-    </div>
+
+    <PopupWithForm name="del" title="Вы уверены?" textButton="Да" />
+
+    <PopupWithForm name="refresh"
+    title="Обновить аватар" textButton="Сохранить">
+      <fieldset className="popup__input-text">
+        <label className="popup__field">
+          <input id="avatar-link-input" className="popup__input popup__input_el_avatar-link" placeholder="Ссылка на картинку" type="url" name="link" required/>
+          <span className="popup__input-error avatar-link-input-error"></span>
+        </label>
+      </fieldset>
+    </PopupWithForm>
+
   </div>
   );
+}
+
+function handleEditAvatarClick() {
+  const popupAvatar = document.querySelector('.popup_refresh');
+  popupAvatar.classList.add('popup_opened');
+}
+
+function handleEditProfileClick() {
+  const popupEdit = document.querySelector('.popup_edd');
+  popupEdit.classList.add('popup_opened');
+}
+
+function handleAddPlaceClick() {
+  const popupAdd = document.querySelector('.popup_add');
+  popupAdd.classList.add('popup_opened');
 }
 
 export default App;
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function PopupWithForm(props) {
   return (
-    <div className="popup popup_del" className={`popup popup_${props.name}`}>
+    <div className={`popup popup_${props.name}`}>
       <div className="popup__container">
         <button className="popup__close-icon" type="button" aria-label="close"></button>
-        <h2 className="popup__heading">Вы уверены?</h2>
+        <h2 className="popup__heading">{props.title}</h2>
         <form className="popup__form" name={props.name} noValidate>
-          <button className="popup__save-button" type="submit">Да</button>
-          {/* Здесь был модификатор popup__save-button_del, нужно не забыть удалить в css и поменять верстку */}
+          {props.children}
+          <button className="popup__save-button" type="submit">{props.textButton}</button>
         </form>
       </div>
     </div>
