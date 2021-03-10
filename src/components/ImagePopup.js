22ed58fc5648660351a3289d3881093eec0154e3
Ник Николос <nic23172@gmail.com>
2021-03-10 10:11:53 +0300

    fix: add uniq alt in PopupImage

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -4,7 +4,7 @@ export default function ImagePopup(props) {
   return (
     <div className={`popup popup_img ${props.card && 'popup_opened'}`}>
       <div className="popup-img">
-        <img className="popup-img__img" src={props.card.link} alt="Загруженная картинка в большем разрешении"/>
+        <img className="popup-img__img" src={props.card.link} alt={`Загруженная картинка в большем разрешении: ${props.card.name}`}/>
         <p className="popup-img__caption">{props.card.name}</p>
         <button className="popup__close-icon" type="button" aria-label="close" onClick={props.onClose}></button>
       </div>
