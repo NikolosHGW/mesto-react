469637e4039ebcd2cb8e218ef1e1ae65a5868e55
Ник Николос <nic23172@gmail.com>
2021-03-10 11:28:06 +0300

    fix: return privious states

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,17 +3,17 @@ import Card from './Card.js';
 import { api } from '../utils/api.js';
 
 export default function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardClick }) {
-  const [userInfo, setUserInfo] = React.useState({});
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState('');
   const [cards, setCards] = React.useState([]);
 
   React.useEffect(() => {
     api.getInfoUser()
       .then(data => {
-        setUserInfo({
-          userName: data.name,
-          userDescription: data.about,
-          userAvatar: data.avatar,
-        })
+        setUserName(data.name);
+        setUserDescription(data.about);
+        setUserAvatar(data.avatar);
       })
       .catch(res => console.log(res));
   }, []);
@@ -30,12 +30,12 @@ export default function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardCl
     <section className="profile">
       <div className="profile__container">
         <div className="profile__avatar-cover">
-          <img className="profile__avatar" src={userInfo.userAvatar} alt="Аватар"/>
+          <img className="profile__avatar" src={userAvatar} alt="Аватар"/>
           <button className="profile__avatar-button" type="button" aria-label="edit" onClick={onEditAvatar}></button>
         </div>
         <div className="profile__info">
-          <h1 className="profile__name">{userInfo.userName}</h1>
-          <p className="profile__job">{userInfo.userDescription}</p>
+          <h1 className="profile__name">{userName}</h1>
+          <p className="profile__job">{userDescription}</p>
           <button className="profile__edit-button" type="button" aria-label="edit" onClick={onEditProfile}></button>
         </div>
       </div>
